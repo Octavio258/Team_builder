2f6cbf24698e7506d501bc6518c9778b52948542
Octavio Politron <octavio.politron@gmail.com>
2022-01-27 19:59:19 -0800

    manager prompt also async function set below

--- index.js
+++ index.js
@@ -15,3 +15,77 @@ const Intern = require('./lib/Intern');
 const generateHTML = require('./src/generateHTML.js')
 
 const teamMembers = [];
+
+const managerprompt = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your managers name?',
+            validate: managerAnswer => {
+                if (managerAnswer) {
+                    return true;
+                } else {
+                    console.log('\nA manager name is required:')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter Manager ID number:',
+            validate: managerAnswer => {
+                if (managerAnswer) {
+                    return true;
+                } else {
+                    console.log('\nManager\'s employee ID number required:')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter Manager email address:',
+            validate: managerAnswer => {
+                if (managerAnswer) {
+                    return true;
+                } else {
+                    console.log('\nManager\'s email address required:')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: 'Enter Manager office number:',
+            validate: managerAnswer => {
+                if (managerAnswer) {
+                    return true;
+                } else {
+                    console.log('\nManager\'s office number required:')
+                    return false;
+                }
+            }
+        }
+    ])
+
+    
+        .then((input) => {
+            const employee = new Manager(input.name, input.id, input.email, input.office);
+            teamMembers.push(employee);
+            teamPick();
+        })
+}
+
+async function init() {
+    try {
+        await managerprompt()
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+init();
