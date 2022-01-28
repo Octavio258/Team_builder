53abb7737b215f2e81a33dab93dd7d9825db44da
Octavio Politron <octavio.politron@gmail.com>
2022-01-27 19:51:51 -0800

    added output paths for html and css with modules requirements for employee types

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const path = require('path')
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "TeamPage.html");
+
+const SRC_DIR = path.resolve(__dirname, "src");
+const srcPath = path.join(SRC_DIR, "styles.css")
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const generateHTML = require('./src/generateHTML.js')
+
+const teamMembers = [];
